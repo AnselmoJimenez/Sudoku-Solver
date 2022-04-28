5263c455537df953bdab64929dd4b33ba8d345de
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-04-28 01:13:19 -0400

    removed tkinter import due to error. Added terminal visual for table. Added more logic to solve.

--- solve.py
+++ solve.py
@@ -1,7 +1,9 @@
 class solve:
     def __init__(self, table):
         self.table = table;
-        self.pos = table[0][0];
+        self.row = 0;
+        self.col = 0;
+        self.pos = self.table[self.row][self.col];
 
     def check_validity(self, entry) -> bool:
         pass
@@ -16,7 +18,28 @@ class solve:
         pass
 
     def insert_entry(self, entry):
-        pass
+        self.table[self.row][self.col] = entry;
 
-    def solve():
-        pass
+    def update_pos(self, row, column):
+        # if at last row, only update column
+        if self.row == 8:
+            self.col += 1;
+
+        # if at end of the column, update row and column
+        elif self.col == 8:
+            self.row += 1;
+            self.col = 0;
+
+        # If at the end of the table
+        elif self.col == 8 and self.row == 8: # end of table
+            pass
+
+    
+    def solve(self, position, row, col):
+        if self.pos != " ": 
+            self.update_pos(self.row, self.column); 
+            self.solve(self.pos, self.row, self.col);
+
+        for i in range(9):
+            self.insert_entry(i);
+            # Validate entry
--- window.py
+++ window.py
@@ -1,4 +1,3 @@
-import tkinter
 
 
 class window:
