73be6a7512e42691d92b3757c360b753d5420b2a
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-04-28 01:46:30 -0400

    Added checking box logic

--- solve.py
+++ solve.py
@@ -3,26 +3,38 @@ class solve:
         self.table = table;
         self.row = 0;
         self.col = 0;
-        self.pos = self.table[self.row][self.col];
 
     def check_validity(self, entry) -> bool:
         pass
 
-    def check_row(self, pos) -> bool:
-        pass
+    def check_row(self, entry) -> bool:
+        for element in self.table[self.row]:
+            if entry == element:
+                return False;
+        return True;
 
-    def check_col(self, pos) -> bool:
-        pass
+    def check_col(self, entry) -> bool:
+        for row in self.table:
+            if row[0] == entry:
+                return False;
+        return True;
 
-    def check_box(self, pos) -> bool:
-        pass
+    def check_box(self, entry) -> bool:
+        box_row = self.row - (self.row % 3);
+        box_col = self.col - (self.col % 3);
+
+        for i in range(9):
+            for j in range(9):
+                if self.table[i + box_row][j + box_col] == entry:
+                    return False;
+        return True;
 
     def insert_entry(self, entry):
         self.table[self.row][self.col] = entry;
 
     def update_pos(self, row, column):
-        # if at last row, only update column
-        if self.row == 8:
+        # If at the end of the table
+        if self.col == 8 and self.row == 8:
             self.col += 1;
 
         # if at end of the column, update row and column
@@ -30,10 +42,9 @@ class solve:
             self.row += 1;
             self.col = 0;
 
-        # If at the end of the table
-        elif self.col == 8 and self.row == 8: # end of table
+        # if at last row, only update column
+        elif self.row == 8: # end of table
             pass
-
     
     def solve(self, position, row, col):
         if self.pos != " ": 
@@ -41,5 +52,5 @@ class solve:
             self.solve(self.pos, self.row, self.col);
 
         for i in range(9):
-            self.insert_entry(i);
+            pass
             # Validate entry
