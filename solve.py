957d0dac4e91b43ad776c545bcbf96584414889a
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-04-28 00:00:01 -0400

    Added supporting functions to solve.py

--- solve.py
+++ solve.py
@@ -1,3 +1,22 @@
 class solve:
     def __init__(self, table):
         self.table = table;
+        self.pos = table[0][0];
+
+    def check_validity(self, entry) -> bool:
+        pass
+
+    def check_row(self, pos) -> bool:
+        pass
+
+    def check_col(self, pos) -> bool:
+        pass
+
+    def check_box(self, pos) -> bool:
+        pass
+
+    def insert_entry(self, entry):
+        pass
+
+    def solve():
+        pass
