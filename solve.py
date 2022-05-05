19cbd1ff067b3158929273542b4c00991a1b42ed
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-05-05 19:58:21 -0400

    Solver working. Needed to remove the tracker of the position. Fix error at end.

--- solve.py
+++ solve.py
@@ -1,9 +1,6 @@
 class solve:
     def __init__(self, table):
         self.table = table;
-        self.row = 0;
-        self.col = 0;
-        self.solved = False;
 
     def draw_table(self, table):
         for i, row in enumerate(table):
@@ -21,73 +18,54 @@ class solve:
                 else:
                     print(f" {col} ", end="|");
             print();
-        print();
 
-    def check_validity(self, entry) -> bool:
-        if self.check_row(entry) and self.check_col(entry) and self.check_box(entry):
+    def check_validity(self, entry, row, col) -> bool:
+        if self.check_row(entry, row) and self.check_col(entry, col) and self.check_box(entry, row, col):
             return True;
         return False;
 
-    def check_row(self, entry) -> bool:
-        for element in self.table[self.row]:
+    def check_row(self, entry, row) -> bool:
+        for element in self.table[row]:
             if entry == element:
                 return False;
         return True;
 
-    def check_col(self, entry) -> bool:
+    def check_col(self, entry, col) -> bool:
         for row in self.table:
-            if row[self.col] == entry:
+            if row[col] == entry:
                 return False;
         return True;
 
-    def check_box(self, entry) -> bool:
-        validity = False;
-        box_row = self.row - (self.row % 3);
-        box_col = self.col - (self.col % 3);
-        print(f"Entry: {entry}");
-        print(f"Box: {box_row // 3} {box_col // 3}");
+    def check_box(self, entry, row, col) -> bool:
+        box_row = row - (row % 3);
+        box_col = col - (col % 3);
 
         for i in range(3):
             for j in range(3):
-                print(f"Checking position: {i + box_row} {j + box_col}");
                 if self.table[i + box_row][j + box_col] == entry:
-                    validity = False;
-                    return validity;
-                else:
-                    validity = True;
-        print();
-        return validity;
+                    return False;
+        return True;
 
-    def insert_entry(self, entry):
-        self.table[self.row][self.col] = entry;
-
-    def update_pos(self):
-        self.col += 1;
-
-        if self.col == 9 and self.row == 9:
-            self.solved = True;
-        elif self.col == 9:
-            self.col = 0;
-            self.row += 1;
-        
-        print(f"current position: {self.row} {self.col}");
+    def insert_entry(self, entry, row, col):
+        self.table[row][col] = entry;
 
     def run(self, row, col):
         self.draw_table(self.table);
 
-        if self.table[self.row][self.col] != 0:
-            self.update_pos();
-            if self.solved != True:
-                self.run(self.row, self.col);
+        if col == 9:
+            col = 0;
+            row += 1;
 
+        print(f"Checking position: {row} {col}");
+
+        if self.table[row][col] != 0:
+            if self.run(row, col + 1):
+                return True;
         else:
             for i in range(9):
-                if self.check_validity(i + 1):
-                    self.insert_entry(i + 1);
-                    self.update_pos();
-                    if self.solved != True:
-                        self.run(self.row, self.col);
-                    else: 
-                        break;
-                else:
-                    
+                if self.check_validity(i + 1, row, col):
+                    self.insert_entry(i + 1, row, col);
+                    if self.run(row, col + 1):
+                        return True;
+                self.table[row][col] = 0;
+        return False;
