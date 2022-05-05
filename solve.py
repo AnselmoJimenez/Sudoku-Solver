59f8eec2c90ae06a537e37132ff38e84273a85c3
Anselmo Jimenez <anselmojimenez0@gmail.com>
2022-05-04 23:41:34 -0400

    Fixed Solving Procedure. Need to implement backtracking.

--- solve.py
+++ solve.py
@@ -3,6 +3,25 @@ class solve:
         self.table = table;
         self.row = 0;
         self.col = 0;
+        self.solved = False;
+
+    def draw_table(self, table):
+        for i, row in enumerate(table):
+            if i % 3 == 0 and i != 0:
+                print("===========❚===========❚===========");
+            elif i != 0:
+                print("---+---+---❚---+---+---❚---+---+---");
+            
+            for j, col in enumerate(row):
+                if col == 0: col = " ";
+                if (j + 1) % 3 == 0 and j != 8:
+                    print(f" {col} ", end="❚");
+                elif j == 8:
+                    print(f" {col} ", end=" ");
+                else:
+                    print(f" {col} ", end="|");
+            print();
+        print();
 
     def check_validity(self, entry) -> bool:
         if self.check_row(entry) and self.check_col(entry) and self.check_box(entry):
@@ -17,44 +36,58 @@ class solve:
 
     def check_col(self, entry) -> bool:
         for row in self.table:
-            if row[0] == entry:
+            if row[self.col] == entry:
                 return False;
         return True;
 
     def check_box(self, entry) -> bool:
+        validity = False;
         box_row = self.row - (self.row % 3);
         box_col = self.col - (self.col % 3);
+        print(f"Entry: {entry}");
+        print(f"Box: {box_row // 3} {box_col // 3}");
 
-        for i in range(9):
-            for j in range(9):
+        for i in range(3):
+            for j in range(3):
+                print(f"Checking position: {i + box_row} {j + box_col}");
                 if self.table[i + box_row][j + box_col] == entry:
-                    return False;
-        return True;
+                    validity = False;
+                    return validity;
+                else:
+                    validity = True;
+        print();
+        return validity;
 
     def insert_entry(self, entry):
         self.table[self.row][self.col] = entry;
 
-    def update_pos(self, row, column):
-        # If at the end of the table
-        if self.col == 8 and self.row == 8:
-            return
+    def update_pos(self):
+        self.col += 1;
 
-        # if at end of the column, update row and column
-        elif self.col == 8:
-            self.row += 1;
+        if self.col == 9 and self.row == 9:
+            self.solved = True;
+        elif self.col == 9:
             self.col = 0;
+            self.row += 1;
+        
+        print(f"current position: {self.row} {self.col}");
 
-        # if at last row, only update column
-        elif self.row == 8: # end of table
-            self.col += 1;
-    
-    def solve(self, row=0, col=0):
-        if self.table[self.row][self.col] != " ": 
-            self.update_pos(self.row, self.col); 
-            self.solve(self.row, self.col);
+    def run(self, row, col):
+        self.draw_table(self.table);
 
-        for i in range(9):
-            if self.check_validity(i):
-                self.insert_entry(i);
-                self.update_pos(self.row, self.col);
-                self.solve(self.row, self.col)
+        if self.table[self.row][self.col] != 0:
+            self.update_pos();
+            if self.solved != True:
+                self.run(self.row, self.col);
+
+        else:
+            for i in range(9):
+                if self.check_validity(i + 1):
+                    self.insert_entry(i + 1);
+                    self.update_pos();
+                    if self.solved != True:
+                        self.run(self.row, self.col);
+                    else: 
+                        break;
+                else:
+                    
